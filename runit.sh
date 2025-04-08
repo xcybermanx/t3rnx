cf43db0eea95e7650a563c2b72ff35034204b9df
xcybermanx <jawad@live.fr>
2025-04-08 10:39:06 +0000

    Update runit.sh

--- runit.sh
+++ runit.sh
@@ -26,7 +26,7 @@ log() {
 }
 
 log "INFO" "1. Update system"
-sudo apt update && sudo apt upgrade -y
+sudo apt update && sudo apt install -y jq
 
 read -p "Enter PRIVATE_KEY_LOCAL: " PRIVATE_KEY_LOCAL
 [[ -z "$PRIVATE_KEY_LOCAL" ]] && { log "ERROR" "PRIVATE_KEY_LOCAL cannot be empty!"; exit 1; }
@@ -37,6 +37,109 @@ read -p "Enter APIKEY_ALCHEMY: " APIKEY_ALCHEMY
 INSTALL_DIR="$HOME/t3rn-v2"
 SERVICE_FILE="/etc/systemd/system/t3rn-executor-v2.service"
 ENV_FILE="/etc/t3rn-executor-v2.env"
+
+mkdir -p "$INSTALL_DIR" && cd "$INSTALL_DIR"
+
+TAG=$(curl -s https://api.github.com/repos/t3rn/executor-release/releases/latest | jq -r '.tag_name')
+[[ -z "$TAG" || "$TAG" == "null" ]] && { log "ERROR" "Failed to get latest version from GitHub!"; exit 1; }
+
+log "INFO" "Using version: $TAG"
+
+EXECUTOR_URL="https://github.com/t3rn/executor-release/releases/download/$TAG/executor-linux-$TAG.tar.gz"
+log "INFO" "Downloading $EXECUTOR_URL"
+
+wget -q --show-progress "$EXECUTOR_URL" -O "executor-linux.tar.gz"
+
+tar -xzf executor-linux.tar.gz || { log "ERROR" "Failed to extract files!"; exit 1; }
+cd executor/executor/bin || { log "ERROR" "Executor directory not found!"; exit 1; }
+
+cat <<EOF | sudo tee "$ENV_FILE" >/dev/null
+RPC_ENDPOINTS='{
+  "l2rn": ["https://t3rn-b2n.blockpi.network/v1/rpc/public", "https://b2n.rpc.caldera.xyz/http"],
+  "arbt": ["https://arbitrum-sepolia.drpc.org", "https://arb-sepolia.g.alchemy.com/v2/$APIKEY_ALCHEMY"],
+  "bast": ["https://base-sepolia-rpc.publicnode.com", "https://base-sepolia.g.alchemy.com/v2/$APIKEY_ALCHEMY"],
+  "mont": ["https://testnet-rpc.monad.xyz], "https://monad-testnet.g.alchemy.com/v2/$APIKEY_ALCHEMY",
+  "opst": ["https://sepolia.optimism.io", "https://opt-sepolia.g.alchemy.com/v2/$APIKEY_ALCHEMY"],
+  "unit": ["https://unichain-sepolia.drpc.org", "https://unichain-sepolia.g.alchemy.com/v2/$APIKEY_ALCHEMY"]
+}'
+EOF
+
+is_port_in_use() { netstat -tuln | grep -q ":$1"; }
+
+PORT=9090
+while is_port_in_use $PORT; do
+    PORT=$((PORT + 1))
+done
+
+log "INFO" "Use port $PORT"
+
+sudo chown -R "$USER:$USER" "$INSTALL_DIR"
+sudo chmod 600 "$ENV_FILE"
+
+cat <<EOF | sudo tee "$SERVICE_FILE" >/dev/null
+[Unit]
+Description=t3rn Executor v2 Service
+After=network.target
+
+[Service]
+User=$USER
+WorkingDirectory=$INSTALL_DIR/executor/executor/bin
+ExecStart=$INSTALL_DIR/executor/executor/bin/executor --port $PORT
+Restart=always
+RestartSec=10
+Environment=ENVIRONMENT=testnet
+Environment=LOG_LEVEL=debug
+Environment=LOG_PRETTY=false
+Environment=EXECUTOR_PROCESS_BIDS_ENABLED=true
+Environment=EXECUTOR_PROCESS_ORDERS_ENABLED=true
+Environment=EXECUTOR_PROCESS_CLAIMS_ENABLED=true
+Environment=EXECUTOR_MAX_L3_GAS_PRICE=500
+Environment=EXECUTOR_MIN_TX_ETH=2
+Environment=EXECUTOR_MAX_TX_GAS=2000000
+Environment=PRIVATE_KEY_LOCAL=$PRIVATE_KEY_LOCAL
+Environment=ENABLED_NETWORKS=arbitrum-sepolia,base-sepolia,optimism-sepolia,l2rn,unichain-sepolia,mont
+EnvironmentFile=$ENV_FILE
+Environment=EXECUTOR_PROCESS_PENDING_ORDERS_FROM_API=true
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+sudo systemctl daemon-reload
+sudo systemctl enable --now t3rn-executor-v2.service
+
+log "SUCCESS" "✅ Executor v2 successfully installed and run!"
+exec sudo journalctl -u t3rn-executor-v2.service -f --no-hostname -o cat
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 mkdir -p "$INSTALL_DIR" && cd "$INSTALL_DIR"
 
